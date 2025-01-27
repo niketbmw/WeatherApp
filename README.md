9a180643ea19b4bc3fac6f2eee56faa82556f19d
niketbmw <niket.jain@bmwtechworks.in>
2025-01-27 12:25:47 +0530

    refact: Migrated to Kotlin 2.0.0 and KSP

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # WeatherApp
-A simple base project for experimentation process.
+This is the simple base project generated for you, so that you can focus more on building the Features, rather than setting up the project.
+
+Few things we have done on the behalf of you 
+- Setting up the Clean Architecture 
+  - Along with `Usecase`, `Repo` and `Presentation` layers with specific Model data classes
+- Setup the DI Graph 
+- Setup the Room and Data Store
+- Retrofit and the Moshi Serialization setup
+- Base Unit Testing
+
+
+## Some Know Blockers 
+- Local Properties SDK Path (It can be different for you)
+- Gradle Version, Use the latest Android Studio and opt for the recommendation option to sync project
+- We are using latest Kotlin Version, so using `KSP` instead of `Kapt`. 
+- Apologies, no Version Catalog being implemented, make sure to take good care of Library Versions you use everywhere.
+- By default the project uses Compose, feel free to use XML Layouts 
+- 
--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,18 +1,19 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    id("org.jetbrains.kotlin.kapt")
-    id("com.google.dagger.hilt.android") version ("2.47")
+    id("com.google.dagger.hilt.android") version ("2.52")
+    id("com.google.devtools.ksp")
+    id("org.jetbrains.kotlin.plugin.compose")
 }
 
 android {
     namespace = "com.bti.weatherapp"
-    compileSdk = 33
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.bti.weatherapp"
         minSdk = 21
-        targetSdk = 33
+        targetSdk = 35
         versionCode = 1
         versionName = "v0.0.1"
 
@@ -53,45 +54,53 @@ android {
         compose = true
     }
 
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1"
-    }
-
-    packagingOptions {
+    packaging {
         resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
-            excludes += "META-INF/LICENSE.txt"
-            excludes += "META-INF/NOTICE.txt"
-            excludes += "LICENSE.txt"
+            excludes.add("/META-INF/{AL2.0,LGPL2.1}")
         }
     }
 }
 
 dependencies {
-    implementation("com.google.dagger:hilt-android:2.47")
-    kapt("com.google.dagger:hilt-compiler:2.47")
-    implementation("androidx.activity:activity-ktx:1.7.2")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-compose")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.6.1")
-    implementation(platform("androidx.compose:compose-bom:2023.06.01"))
-    implementation("androidx.compose.material3:material3")
+    // Core
     implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.material3:material3")
+    implementation("androidx.activity:activity-ktx:1.10.0")
     implementation("androidx.compose.ui:ui-graphics")
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("javax.inject:javax.inject:1")
+
+    // Hilt
+    implementation("com.google.dagger:hilt-android:2.52")
+    ksp("com.google.dagger:hilt-compiler:2.52")
+
+    // Lifecycle
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.7")
+    implementation("androidx.lifecycle:lifecycle-runtime-compose")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.7")
+
+    // Compose
+    implementation(platform("androidx.compose:compose-bom:2025.01.00"))
     implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
-    implementation("com.google.android.material:material:1.9.0")
-    implementation("androidx.core:core-ktx:1.10.1")
-    testImplementation("junit:junit:4.13.2")
+    implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
+    api("androidx.compose.runtime:runtime:1.7.6")
+
+    // Coroutines
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
-    implementation("javax.inject:javax.inject:1")
+
+    // Retrofit + Moshi
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-moshi:2.4.0")
-    kapt("androidx.room:room-compiler:2.5.2")
-    implementation("androidx.room:room-runtime:2.5.2")
-    implementation("androidx.room:room-ktx:2.5.2")
-    implementation("androidx.datastore:datastore-preferences:1.0.0")
-    api("androidx.compose.runtime:runtime:1.5.0")
+    ksp("androidx.room:room-compiler:2.6.1")
+
+    // Persistence
+    implementation("androidx.room:room-runtime:2.6.1")
+    implementation("androidx.room:room-ktx:2.6.1")
+    implementation("androidx.datastore:datastore-preferences:1.1.2")
+
+    // Test
+    testImplementation("junit:junit:4.13.2")
 }
